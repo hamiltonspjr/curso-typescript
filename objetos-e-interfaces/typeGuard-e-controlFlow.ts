7c51167e6ee5d294a472a6430ba345e3119466ac
Hamilton Junior <hamilton-souza@outlook.com.br>
2023-02-13 11:22:11 -0300

    completed class on type guard and control flow

---
+++ objetos-e-interfaces/typeGuard-e-controlFlow.ts
@@ -0,0 +1,49 @@
+function typeGuard(value: any) {
+  if (typeof value === "string") {
+    return value.toLowerCase();
+  }
+  if (typeof value === "number") {
+    return value.toFixed(1);
+  }
+  if (value instanceof HTMLElement) {
+    return value.innerText;
+  }
+}
+
+console.log(typeGuard(200.0555));
+console.log(typeGuard("TESTE"));
+console.log(typeGuard(document.body));
+
+// IN
+
+const obj = {
+  nome: "Origamid",
+};
+
+if ("nome" in obj) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+interface Produto {
+  nome: string;
+  preco: number;
+}
+
+async function fetchProduto() {
+  const response = await fetch("https://api.origamid.dev/json/notebook.json");
+  const json = await response.json();
+  handleProduto(json);
+}
+
+function handleProduto(data: Produto) {
+  if ("preco" in data && "nome" in data) {
+    document.body.innerHTML += `
+      <p>Nome: ${data.nome}</p>
+      <p>Preço: R$ ${data.preco + 100}</p>
+    `;
+  }
+}
+
+fetchProduto();
