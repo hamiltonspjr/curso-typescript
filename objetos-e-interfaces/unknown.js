34d049d13a08ae3ee3d06992bde212a372a47ecc
Hamilton Junior <hamilton-souza@outlook.com.br>
2023-02-13 11:41:58 -0300

    completed class on unknown

---
+++ objetos-e-interfaces/unknown.js
@@ -0,0 +1,15 @@
+"use strict";
+function typeGuard2(value) {
+    if (typeof value === "string") {
+        return value.toLowerCase();
+    }
+    if (typeof value === "number") {
+        return value.toFixed();
+    }
+    if (value instanceof HTMLElement) {
+        return value.innerText;
+    }
+}
+console.log(typeGuard2("ORIGAMID"));
+console.log(typeGuard2(200));
+console.log(typeGuard2(document.body));
